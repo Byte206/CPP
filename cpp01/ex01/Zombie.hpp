8bcf09e39e761e39aec07cf8c3fe60fa0f8d3c77
gabi <189730327+Byte206@users.noreply.github.com>
2026-01-27 09:22:03 +0100

    01/ex02 done

---
+++ cpp01/ex01/Zombie.hpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+class Zombie {
+public:
+    Zombie();
+    Zombie(std::string name);
+    ~Zombie();
+    void announce();
+    void setName(std::string name);
+
+private:
+    std::string name;
+};
